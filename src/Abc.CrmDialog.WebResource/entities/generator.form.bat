52ac44bbb256d30373120d615f0abee179d19eb7
PhuocLe <vanphuoc@gmail.com>
2020-07-09 14:28:35 +0700

    Init Solution

---
+++ src/Abc.CrmDialog.WebResource/entities/generator.form.bat
@@ -0,0 +1,9 @@
+@echo off
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
+    set DynamicsCrmDevKitCli=%%d
+    goto break
+)
+:break
+set CrmConnection="AuthType=ClientSecret;Url=https://org581e92dc.crm5.dynamics.com;ClientId=ac758a0d-4aee-4763-b91a-5c269d2dd983;ClientSecret=YnoI6CA-3-v4RWSHqm~5u92i43n_0S71__;"
+"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\..\DynamicsCrm.DevKit.Cli.json" /type:"generators" /profile:"JS-FORM"
+exit
