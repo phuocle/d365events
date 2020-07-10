6fabb19a034490788b8d7f4b784534ee312f2f1a
PhuocLe <vanphuoc@gmail.com>
2020-07-10 13:52:10 +0700

    Ready for the event

---
+++ src/Abc.CrmDialog.WebResource/entities/Account.Dialog.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+var dialog = (function () {
+    "use strict";
+    async function onOpen(executionContext) {
+        alert("HELLO WORLD");
+    }
+    return {
+        OnOpen: onOpen
+    };
+})();
